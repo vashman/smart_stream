8fcb715dffecc91e2cda3730b09ca6c6b0be9715
Sunny S <3030712@gmail.com>
2014-10-27 01:54:42 -0700

    changed template parameter style.

--- include/unique_ostream.hpp
+++ include/unique_ostream.hpp
@@ -18,36 +18,36 @@ base class's rdbuf(streambuf*), which will allow a client to use a
 unmanged Buffer.
 
 The unique_stream takes the following template arguments;
-1. CharT, char type used by streambuf and osstream.
+1. charT, char type used by streambuf and osstream.
 
-2. Traits, char traits used by this, streambuf and ostream, defaulting to
-   std::char_traits<CharT>.
+2. traits, char traits used by this, streambuf and ostream, defaulting to
+   std::char_traits<charT>.
 
 3. Deleter, unique_ptr deleter for the above Buffer pointer,
    defaulting to std::default_delete.
 */
 template <
-  typename CharT
-, typename Traits = std::char_traits<CharT>
+  typename charT
+, typename traits = std::char_traits<charT>
 , typename Deleter = std::default_delete <
-    std::basic_streambuf<CharT,Traits>
+    std::basic_streambuf<charT,traits>
   >
 >
-class  basic_unique_ostream : public std::basic_ostream<CharT,Traits>{
+class  basic_unique_ostream : public std::basic_ostream<charT,traits>{
 public:
-  typedef std::unique_ptr<std::basic_streambuf<CharT,Traits>
+  typedef std::unique_ptr<std::basic_streambuf<charT,traits>
                                                    , Deleter>   ptr_type;
 
-  typedef typename std::basic_ostream<CharT,Traits>::char_type char_type;
+  typedef typename std::basic_ostream<charT,traits>::char_type char_type;
 
-  typedef typename std::basic_ostream<CharT,Traits>::traits_type
+  typedef typename std::basic_ostream<charT,traits>::traits_type
                                                            traits_type;
 
-  typedef typename std::basic_ostream<CharT,Traits>::int_type int_type;
+  typedef typename std::basic_ostream<charT,traits>::int_type int_type;
 
-  typedef typename std::basic_ostream<CharT,Traits>::pos_type pos_type;
+  typedef typename std::basic_ostream<charT,traits>::pos_type pos_type;
 
-  typedef typename std::basic_ostream<CharT,Traits>::off_type off_type;
+  typedef typename std::basic_ostream<charT,traits>::off_type off_type;
 
   explicit
   basic_unique_ostream(
@@ -60,25 +60,25 @@ public:
   */
   explicit
   basic_unique_ostream(
-   std::basic_streambuf<CharT,Traits> * const
+   std::basic_streambuf<charT,traits> * const
   );
 
   basic_unique_ostream(
-   basic_unique_ostream<CharT,Traits,Deleter> &&
+   basic_unique_ostream<charT,traits,Deleter> &&
   );
 
-  basic_unique_ostream<CharT,Traits,Deleter> &
+  basic_unique_ostream<charT,traits,Deleter> &
   operator=(
-   basic_unique_ostream<CharT,Traits,Deleter> &&
+   basic_unique_ostream<charT,traits,Deleter> &&
   );
 
   basic_unique_ostream(
-   basic_unique_ostream<CharT,Traits,Deleter> const &
+   basic_unique_ostream<charT,traits,Deleter> const &
   ) = delete;
 
-  basic_unique_ostream<CharT,Traits,Deleter> &
+  basic_unique_ostream<charT,traits,Deleter> &
   operator=(
-   basic_unique_ostream<CharT,Traits,Deleter> const &
+   basic_unique_ostream<charT,traits,Deleter> const &
   ) = delete;
 
   ~basic_unique_ostream();
@@ -103,7 +103,7 @@ public:
   Sets the underlying stream buffer to this->uqptr pointer,
   while returning previous pointer vid ostream.rdbuf().
   */
-  std::basic_streambuf<CharT,Traits> *
+  std::basic_streambuf<charT,traits> *
   rebind();
 
 private:
